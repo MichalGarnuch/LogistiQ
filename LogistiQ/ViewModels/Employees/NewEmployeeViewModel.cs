a0ae431c3d2a7a7abe7fedb85d86869d90129a06
Michal <m.p.garnuch@gmail.com>
2025-01-25 23:46:06 +0100

    NewEmployee have validation

--- LogistiQ/ViewModels/Employees/NewEmployeeViewModel.cs
+++ LogistiQ/ViewModels/Employees/NewEmployeeViewModel.cs
@@ -1,16 +1,19 @@
 ﻿using LogistiQ.Models.Entities;
 using LogistiQ.Models.EntitiesForView.BaseWorkspace;
+using LogistiQ.Validators;
 using LogistiQ.ViewModels.BaseWorkspace;
 using LogistiQ.Views.BaseWorkspace;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace LogistiQ.ViewModels.Employees
 { 
-    public class NewEmployeeViewModel : SingleRecordViewModel<LogistiQ.Models.Entities.Employees>
+    public class NewEmployeeViewModel : SingleRecordViewModel<LogistiQ.Models.Entities.Employees>, IDataErrorInfo
     {
 
         #region Konstruktor
@@ -116,6 +119,53 @@ namespace LogistiQ.ViewModels.Employees
         {
             logistiQ_Entities.Employees.Add(item);//dodaje towar do lokalnej kolekcji
             logistiQ_Entities.SaveChanges();//zapisuje zmiany dokonane w bazie danych
+        }
+
+        #endregion
+
+        #region Validation
+
+        public string Error => string.Empty;
+        // Słownik przechowujący komunikaty błędów dla każdej właściwości
+        private readonly Dictionary<string, string> _validationMessages = new Dictionary<string, string>();
+
+        public string this[string properties]
+        {
+            get
+            {
+                string validateMessage = string.Empty;
+
+                if (properties == nameof(FirstName))
+                {
+                    validateMessage = StringValidator.ValidateIsFirstLetterUpper(FirstName);
+                }
+                else if (properties == nameof(LastName))
+                {
+                    validateMessage = StringValidator.ValidateIsFirstLetterUpper(LastName);
+                }
+                else if (properties == nameof(WarehouseID))
+                {
+                    validateMessage = StringValidator.ValidateIsNotEmpty(WarehouseID?.ToString());
+                }
+
+                // Aktualizujemy słownik błędów
+                if (!string.IsNullOrEmpty(validateMessage))
+                {
+                    _validationMessages[properties] = validateMessage;
+                }
+                else
+                {
+                    _validationMessages.Remove(properties);
+                }
+
+                return validateMessage;
+            }
+        }
+
+        public override bool IsValid()
+        {
+            // Jeśli w słowniku nie ma błędów, wszystkie pola są poprawne
+            return !_validationMessages.Any();
         }
 
         #endregion
