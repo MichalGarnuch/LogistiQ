d64c583a610ff4b48c87869c4959b4b922f69818
Michal <m.p.garnuch@gmail.com>
2025-01-31 00:08:25 +0100

    Przygotowany interfeac do WarehousesOverview

---
+++ LogistiQ/Views/BusinessLogicView/WarehouseOverviewView.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LogistiQ.Views.BusinessLogicView
+{
+    /// <summary>
+    /// Interaction logic for WarehouseOverviewView.xaml
+    /// </summary>
+    public partial class WarehouseOverviewView : UserControl
+    {
+        public WarehouseOverviewView()
+        {
+            InitializeComponent();
+        }
+    }
+}
